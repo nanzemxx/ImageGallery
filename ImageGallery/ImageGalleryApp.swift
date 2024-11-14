6f45cbadcd8f4a442c7e2ca5c61a23376084f4d3
xinxiong <xiongxinstu@icloud.com>
2024-11-14 03:33:12 +0100

    Initial Commit

---
+++ ImageGallery/ImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGalleryApp.swift
+//  ImageGallery
+//
+//  Created by xiongxin on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
